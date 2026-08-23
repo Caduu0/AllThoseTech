2116635c8111b7cd7b061c42baedd6b18c0434fd
Caduu0 <caduzinbr2020@gmail.com>
2026-08-23 12:00:53 -0300

    atualizacao para 0.0.8; e remocao das receitas appflux

--- kubejs/server_scripts/recipes/adv_ext_ae2.js
+++ kubejs/server_scripts/recipes/adv_ext_ae2.js
@@ -1,65 +1,5 @@
 // -= Crystal Assembler (ExtendedAE). =-
 ServerEvents.recipes(event => {
-    // Energy Processor
-    event.custom({
-        type: 'extendedae:crystal_assembler',
-        energy: 2000, // Energia (AE2).
-
-        // Ingredientes (de 1 a 9 itens).
-        input_items: [
-            {
-                amount: 4, 
-                ingredient: 'appflux:printed_energy_processor'
-            },
-            {
-                amount: 4,
-                ingredient: 'ae2:printed_silicon'
-            },
-            {
-                amount: 4,
-                ingredient: 'minecraft:redstone'
-            }
-        ],
-
-        // Resultado.
-        output: {
-            id: 'appflux:energy_processor',
-            count: 4
-        }
-    })
-    // Redstone Crystal
-    event.custom({
-        type: 'extendedae:crystal_assembler',
-        energy: 9000, // Energia (AE2).
-
-        // Ingredientes (de 1 a 9 itens).
-        input_items: [
-            {
-                amount: 16, 
-                ingredient: 'minecraft:redstone_block'
-            },
-            {
-                amount: 16, 
-                ingredient: 'ae2:fluix_crystal'
-            },
-            {
-                amount: 16, 
-                ingredient: 'minecraft:glowstone_dust'
-            }
-        ],
-
-        // Liquido.
-        input_fluid: {
-            ingredient: 'minecraft:water',
-            amount: 1000 // Quantidade (mB).
-        },
-
-        // Resultado.
-        output: {
-            id: 'appflux:redstone_crystal',
-            count: 16
-        }
-    })
     // Vibranium Allthemodium Alloy Ingot
     event.custom({
         type: 'extendedae:crystal_assembler',
@@ -111,22 +51,6 @@ ServerEvents.recipes(event => {
         // Resultado
         output: {
             id: 'advanced_ae:printed_quantum_processor',
-            count: 9
-        }
-    })
-    // Printed Energy Circuit
-    event.custom({
-        type: 'extendedae:circuit_cutter',
-        energy: 18000, // Energia (AE2).
-    
-        // Ingrediente.
-        input: {
-            ingredient: 'appflux:charged_redstone_block'
-        },
-      
-        // Resultado
-        output: {
-            id: 'appflux:printed_energy_processor',
             count: 9
         }
     })
